99bd4b8e8040357743bb72265d4aea27a4ba4ed4
morganhawkins <66576505+morganhawkins@users.noreply.github.com>
2025-06-27 13:07:47 -0400

    adding binary search dat relevance search and test cases

--- src/read_data/data_types.rs
+++ src/read_data/data_types.rs
@@ -1,6 +1,6 @@
-use serde::Deserialize;
+use core::time;
 
-use crate::read_data::read_underlying;
+use serde::Deserialize;
 
 #[derive(Deserialize, Debug)]
 pub struct KalshiRecord {
@@ -28,9 +28,69 @@ impl KalshiDataset {
         KalshiDataset { records, strike, start_ts, expir_ts }
     }
 
-    pub fn relevant_slice<'a>(&self, underlying_data: &'a Vec<CoinbaseRecord>) -> &'a [CoinbaseRecord]{
+    // finds the CoinbaseRecord with the largest index s.t. it's ts field is still <= `timestamp`
+    pub fn find_idx(timestamp: u64, underlying_data: &Vec<CoinbaseRecord>) -> Option<usize> {
 
-        return &underlying_data[1..3];
+        // boundary check - if timestamp if < first element or >= last element,
+        //  we cannot find a satisfying element. Otherwise we can definitely find an element
+        {   
+            let first_record = match underlying_data.get(0){
+                Some(val) => val,
+                None => return None
+            };
+            let last_record = match underlying_data.last(){
+                Some(val) => val,
+                None => return None
+            };
+
+            if timestamp < first_record.ts || timestamp >= last_record.ts {
+                return None
+            }
+        }
+
+        // binary search view start idx, view end idx, and current idx
+        let mut range_start = 0_usize;
+        let mut range_end = underlying_data.len();
+        let mut idx = underlying_data.len() / 2_usize;
+
+        let mut idx_ts: u64;
+        let mut next_idx_ts: u64;
+        loop {
+            // current idx timestamp and next item timestamp update
+            idx_ts = underlying_data[idx].ts;
+            next_idx_ts = underlying_data[idx+1].ts;
+
+            // check if search condition satisfied
+            if idx_ts <= timestamp && next_idx_ts > timestamp {
+                return Some(idx);
+            } 
+
+            // change search range to upper half or lower half
+            if idx_ts <= timestamp {
+                // if idx ts is still lower than `timestamp` -> look at upper half
+                range_start = idx;
+            } else {
+                // if idx ts is too high -> look at lower half
+                range_end = idx;
+            }
+            // update idx to be in middle of range
+            idx = (range_start + range_end) / 2_usize;
+
+        }
+    }
+
+    // NOTE: assuming that vector is sorted on `ts` field of CoinbaseRecord
+    pub fn relevant_slice<'a>(&self, underlying_data: &'a Vec<CoinbaseRecord>) -> Option<&'a [CoinbaseRecord]>{
+        let slice_start = Self::find_idx(self.start_ts, underlying_data)?;
+        let slice_end = Self::find_idx(self.expir_ts, underlying_data)?;
+        
+        // panic in case of unexpected behavior
+        if slice_end <= slice_start {
+            panic!("Data collection issue, underlying data is unsorted.");
+        }
+        
+        // can safely add 1 to end_slice since it will never be last element
+        return Some(&underlying_data[slice_start..slice_end+1usize]);
     }
 
 }
@@ -42,5 +102,48 @@ pub struct CoinbaseRecord {
     pub price: f64, // open price
     pub mu: f64, // mean log return _h rolling window
     pub sigma: f64, // sigma log return _h rolling window
+}
+
+impl CoinbaseRecord {
+    pub fn new(ts: u64, price: f64, mu: f64, sigma: f64) -> Self{
+        Self {ts, price, mu, sigma}
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn create_mock_cb_records(ts_start: u64, ts_end: u64) -> Vec<CoinbaseRecord>{
+        let mut mock = Vec::new();
+        for ts in ts_start..ts_end {
+            mock.push(
+                CoinbaseRecord::new(ts*2_u64, 0.0, 0.0, 0.0)
+            );
+        }
+        mock
+    }
+ 
+    #[test]
+    fn test_binary_underlying_records() {
+        let mock_cb_records = create_mock_cb_records(2,8);
+        
+        // test non-exact match
+        let start_record_idx = KalshiDataset::find_idx(7, &mock_cb_records);
+        assert!(start_record_idx.unwrap() == 1usize);
+        // test exact match 
+        let start_record_idx = KalshiDataset::find_idx(8, &mock_cb_records);
+        assert!(start_record_idx.unwrap() == 2usize);
+        // test out-of-bounds upper
+        let start_record_idx = KalshiDataset::find_idx(20, &mock_cb_records);
+        assert!(start_record_idx.is_none());
+        // test out-of-bounds lower
+        let start_record_idx = KalshiDataset::find_idx(0, &mock_cb_records);
+        assert!(start_record_idx.is_none());
+    }
+
+
+
 }
 
--- src/read_data/read_kalshi.rs
+++ src/read_data/read_kalshi.rs
@@ -11,13 +11,13 @@ fn chunk_kalshi_records<'a>(text: &'a String, re: &Regex) -> impl Iterator<Item 
     chunks
 }
 
-pub fn read_kalshi<P: AsRef<Path>>(path: P) -> Result<Vec<KalshiRecord>, Box<dyn Error>>{
+pub fn read_kalshi<P: AsRef<Path>>(path: P) -> Result<KalshiDataset, Box<dyn Error>>{
     // compile regex to chunk array into objects
     let pattern= r"\{.*?\}";
     let re = Regex::new(pattern).unwrap(); 
 
     // read to string and chunk
-    let contents = read_to_string(path)?;
+    let contents = read_to_string(&path)?;
     let chunks = chunk_kalshi_records(&contents, &re);
     
     // TODO: remove unwrap to handle deserialization error
@@ -25,9 +25,19 @@ pub fn read_kalshi<P: AsRef<Path>>(path: P) -> Result<Vec<KalshiRecord>, Box<dyn
     
     // getting meta data for dataset
     let expir_ts = (records[0].tte*(3600_f64)) as u64 + records[0].ts;
+    let start_ts = records[0].ts;
+    let strike_fn = match path.as_ref().file_name() {
+        Some(val) => val,
+        None => return Err(format!("unable to turn `path` into string: {:?}", path.as_ref()).into()),
+    };
+    let strike_split_str: Vec<&str> = strike_fn.to_str().unwrap().split(".").into_iter().collect();
+    let strike = match str::parse::<f64>(strike_split_str[0]){
+        Ok(val) => val,
+        Err(_) => return Err(format!("unable to parse file name w/o extenion into f64 strike price: {:?}", strike_split_str).into()),
+    };
     
-
-    Ok(records)
+    let dataset = KalshiDataset{records, strike, start_ts, expir_ts};
+    Ok(dataset)
 }
 
 
