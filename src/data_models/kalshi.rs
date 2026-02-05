cb4de812243f7fa2f28bf6f15bed983a257a2b73
morganhawkins <66576505+morganhawkins@users.noreply.github.com>
2026-02-04 21:04:54 -0500

    implement from kalshi-rs type for kalshi record update

--- src/data_models/kalshi.rs
+++ src/data_models/kalshi.rs
@@ -1,22 +1,37 @@
+use chrono::DateTime;
+use kalshi_rs::websocket::models::OrderbookDeltaMessage;
 use serde::Deserialize;
+
 use super::tick::Tick;
 
 #[derive(Deserialize, Debug)]
-pub struct KalshiRecord {
+pub struct KalshiOrderbookUpdate {
     pub ts: u64, // unix time stamp
-    pub tte: f64, // hours until contract expiration
-    pub bid: u32, // bid price, in hundreths of a cent
-    pub ask: u32, // ask price, in hundreths of a cent
+    pub price: u64, // price in hundreths of a cent
+    pub delta: i64, // change in contract amount at price level
 }
 
-impl KalshiRecord {
-    pub fn new(ts: u64, tte: f64, bid: u32, ask: u32) -> Self {
-        KalshiRecord { ts, tte, bid, ask }
+impl KalshiOrderbookUpdate {
+    pub fn new(ts: u64, price: u64, delta: i64) -> Self {
+        KalshiOrderbookUpdate { ts, price, delta }
     }
 }
 
-impl Tick for KalshiRecord {
+impl Tick for KalshiOrderbookUpdate {
     fn ts(&self) -> u64 {
         self.ts
+    }
+}
+
+impl TryFrom<OrderbookDeltaMessage> for KalshiOrderbookUpdate {
+    type Error = Box<dyn std::error::Error>;
+
+    fn try_from(message: OrderbookDeltaMessage) -> Result<Self, Self::Error> {
+        let ts = DateTime::parse_from_rfc3339(
+            &message.ts
+        )?.timestamp() as u64;
+        let price = message.price as u64;
+        let delta = message.delta;
+        Ok(KalshiOrderbookUpdate { ts, price, delta })
     }
 }
